bb8d2a6ad1bf1acb3649cefe737f5c5b583d7512
JohnAmican <amicangelo.j@husky.neu.edu>
2013-01-25 20:35:59 -0500

    generated with js instead of coffeescript

---
+++ app/assets/javascripts/rewindr.js
@@ -0,0 +1,13 @@
+window.Rewindr = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Rewindr.initialize();
+});
--- app/assets/javascripts/rewindr.js.coffee
+++
@@ -1,9 +0,0 @@
-window.Rewindr =
-  Models: {}
-  Collections: {}
-  Views: {}
-  Routers: {}
-  initialize: -> alert 'Hello from Backbone!'
-
-$(document).ready ->
-  Rewindr.initialize()
