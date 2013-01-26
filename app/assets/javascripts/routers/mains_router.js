b8f3830d0e22ea0900edc4bf05bff06bfa17e9e4
Robert O'Neill <rjroneill@gmail.com>
2013-01-25 23:30:46 -0500

    Added the route for a playlist.

--- app/assets/javascripts/routers/mains_router.js
+++ app/assets/javascripts/routers/mains_router.js
@@ -1,10 +1,18 @@
 Rewindr.Routers.Mains = Backbone.Router.extend({
   routes: {
-    '': 'index'
+    '': 'index',
+    "playlist/:userName" : "playlist"
   },
 
   index: function() {
      var view = new Rewindr.Views.MainsIndex()
-     $('#container').html(view.render().el)
+     $('#container').html(view.render().el);
+  },
+
+  playlist: function(userName) {
+  	debugger;
+  	var view = new Rewindr.Views.PlaylistsIndex();
+  	$('#container').html(view.render().el);
+  	$('#container').append(userName);
   }
 });
