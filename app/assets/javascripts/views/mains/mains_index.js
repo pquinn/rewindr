2b977523385f1067b365d932753e8f7bea61b729
Robert O'Neill <rjroneill@gmail.com>
2013-01-25 22:42:25 -0500

    Added a basic main page.

--- app/assets/javascripts/views/mains/mains_index.js
+++ app/assets/javascripts/views/mains/mains_index.js
@@ -3,7 +3,23 @@ Rewindr.Views.MainsIndex = Backbone.View.extend({
   template: JST['mains/index'],
   
   render: function() {
-    $(this.el).html(this.template())
+  	var months = {
+  		0: "January",
+  		1 : "February",
+  		2 : "March",
+  		3 : "April",
+  		4 : "May",
+  		5 : "June",
+  		6 : "July",
+  		7 : "August",
+  		8 : "September",
+  		9 : "October",
+  		10 : "November",
+  		11 : "December"
+  	};
+  	var date = new Date();
+  	var dateString = months[date.getMonth()] + " " + date.getDate() + ", " + (date.getFullYear() - 1);
+    $(this.el).html(this.template({"date" : dateString}))
     return this
   }
 
