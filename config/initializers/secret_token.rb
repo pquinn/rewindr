fa0365ea4693309528388ab7ac4f883c0463b2e5
JohnAmican <amicangelo.j@husky.neu.edu>
2013-01-25 19:05:51 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rewindr::Application.config.secret_token = 'ad21244037a479d9d95aee09af7081cd068eb180093631c933b3ba0f7801d52a5da4c746483456f0b772a20bec526bcb921a18a2689276a8e8661ce78b6e763a'
