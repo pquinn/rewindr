56587bc02d95d2449543002bdf0a720d66d48855
JohnAmican <amicangelo.j@husky.neu.edu>
2013-02-11 11:40:06 -0500

    tiny refactoring in LastfmHelper

--- lib/lastfm_helper.rb
+++ lib/lastfm_helper.rb
@@ -1,17 +1,15 @@
 class LastfmHelper
-  def self.get_recent_tracks(options={})
-    tracks = []
-    
+  def self.get_recent_tracks(options={})    
     begin
-      tracks = LASTFM.user.get_recent_tracks(
-                 :user  => options[:user_name], 
-                 :from  => options[:from] || 365.days.ago.to_i,
-                 :to    => options[:to]   || 364.days.ago.to_i,
-                 :page  => options[:page],
-                 :limit => options[:limit] || 200
-               ) || []
+      LASTFM.user.get_recent_tracks(
+         :user  => options[:user_name], 
+         :from  => options[:from] || 365.days.ago.to_i,
+         :to    => options[:to]   || 364.days.ago.to_i,
+         :page  => options[:page],
+         :limit => options[:limit] || 200
+       ) || []
     rescue Lastfm::ApiError
-      tracks
+      []
     end
   end
 end
